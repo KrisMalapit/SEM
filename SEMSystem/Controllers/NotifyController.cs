67825937d8fd001de6bf8a6d82218d351da66ed5
Kristoffer Malapit <krismalapit@yahoo.com>
2021-06-07 16:27:00 +0800

    update

--- SEMSystem/Controllers/NotifyController.cs
+++ SEMSystem/Controllers/NotifyController.cs
@@ -70,7 +70,12 @@ namespace SEMSystem.Controllers
 
 
             string status = "";
-            string message = SendEmail(notify);
+            
+            string message = "";
+            if (docstatus == "Approved")
+            {
+                message = SendEmail(notify);
+            }
 
             if (message != "success")
             {
